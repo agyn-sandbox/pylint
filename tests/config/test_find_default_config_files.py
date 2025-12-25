34bfd601fb604291cfe449878cce3923244f7f61
Rowan Stein <rowan.stein@agyn.io>
2025-12-25 19:06:59 +0000

    tests: restore long-option verbose test body; keep new -v test separate

--- tests/config/test_find_default_config_files.py
+++ tests/config/test_find_default_config_files.py
@@ -148,7 +148,6 @@ def test_pylintrc_parentdir_no_package() -> None:
 @pytest.mark.usefixtures("pop_pylintrc")
 def test_verbose_output_no_config(capsys: CaptureFixture) -> None:
     """Test that we print a log message in verbose mode with no file."""
-
 @pytest.mark.usefixtures("pop_pylintrc")
 def test_verbose_output_no_config_short(capsys: CaptureFixture) -> None:
     """Test that we print a log message in verbose mode with -v and no file."""
@@ -159,16 +158,6 @@ def test_verbose_output_no_config_short(capsys: CaptureFixture) -> None:
             os.chdir(chroot_path / "a/b/c")
             with pytest.raises(SystemExit):
                 Run(["-v"])
-            out = capsys.readouterr()
-            assert "No config file found, using default configuration" in out.err
-
-    with tempdir() as chroot:
-        with fake_home():
-            chroot_path = Path(chroot)
-            testutils.create_files(["a/b/c/d/__init__.py"])
-            os.chdir(chroot_path / "a/b/c")
-            with pytest.raises(SystemExit):
-                Run(["--verbose"])
             out = capsys.readouterr()
             assert "No config file found, using default configuration" in out.err
 
